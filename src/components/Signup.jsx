3caa782d2e17ae4c5db42f666be4351fbab71f99
Yash Bhakta <ybhaktaace11@gmail.com>
2026-04-15 14:16:58 +0530

    update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './Login.css'
+import './login.css'
 import { Link, useNavigate } from 'react-router'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
